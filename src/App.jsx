8da18a53e450ea2030926598d7c3911f15b734df
pejhanbio-cmd <pejhan79s@gmail.com>
2026-09-14 01:12:40 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
 import Biology10 from "./pages/Biology10";
+import Biology10Chapter1 from "./pages/Biology10Chapter1";
 function PlaceholderPage({ title }) {
   return (
     <>
@@ -40,6 +41,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
 <Route
+  path="/courses/biology-10/chapter-1"
+  element={<Biology10Chapter1 />}
+/>
+        <Route
   path="/courses/biology-10"
   element={<Biology10 />}
 />
