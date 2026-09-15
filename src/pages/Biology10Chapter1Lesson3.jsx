15f8e87de4e5810cf3d11eadeb42c80110183a37
pejhanbio-cmd <pejhan79s@gmail.com>
2026-09-15 14:34:03 +0330

    Add Biology10Chapter1Lesson3 component

---
+++ src/pages/Biology10Chapter1Lesson3.jsx
@@ -0,0 +1,13 @@
+import FigureNotesChapter1 from "../components/biology10/FigureNotesChapter1";
+
+function Biology10Chapter1Lesson3() {
+  return (
+    <main>
+      <section className="container">
+        <FigureNotesChapter1 />
+      </section>
+    </main>
+  );
+}
+
+export default Biology10Chapter1Lesson3;
