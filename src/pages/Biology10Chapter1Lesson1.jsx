edaac566c43008d6336de76bbccac0b78abd40e8
pejhanbio-cmd <pejhan79s@gmail.com>
2026-09-15 14:17:19 +0330

    Update Biology10Chapter1Lesson1.jsx

--- src/pages/Biology10Chapter1Lesson1.jsx
+++ src/pages/Biology10Chapter1Lesson1.jsx
@@ -8,6 +8,7 @@ import {
   Microscope,
   Sprout,
 } from "lucide-react";
+import FigureNotesChapter1 from "../components/biology10/FigureNotesChapter1";
 
 const sections = [
   {
@@ -276,6 +277,7 @@ function Biology10Chapter1Lesson1() {
             </div>
           </aside>
         </div>
+        <FigureNotesChapter1 />
       </section>
     </main>
   );
