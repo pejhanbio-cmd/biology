5a03c10ad994fc770db9bdaf2c38c644ddfe4bad
pejhanbio-cmd <pejhan79s@gmail.com>
2026-09-16 20:46:55 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,9 @@ import Health12 from "./pages/Health12";
 
 import Biology10Chapter1 from "./pages/Biology10Chapter1";
 import Biology10Chapter1Lesson1 from "./pages/Biology10Chapter1Lesson1";
+import Biology10Chapter1Lesson2 from "./pages/Biology10Chapter1Lesson2";
 import Biology10Chapter1Lesson3 from "./pages/Biology10Chapter1Lesson3";
+
 export default function App() {
   return (
     <>
@@ -28,49 +30,33 @@ export default function App() {
         <Route path="/" element={<Home />} />
 
         <Route path="/courses" element={<Courses />} />
-
         <Route path="/resources" element={<Resources />} />
-
         <Route path="/tests" element={<Tests />} />
-
         <Route path="/about" element={<About />} />
-
         <Route path="/contact" element={<Contact />} />
-
         <Route path="/privacy" element={<Privacy />} />
 
-        <Route
-          path="/courses/biology-10"
-          element={<Biology10 />}
-        />
-
+        <Route path="/courses/biology-10" element={<Biology10 />} />
         <Route
           path="/courses/biology-10/chapter-1"
           element={<Biology10Chapter1 />}
         />
-
         <Route
           path="/courses/biology-10/chapter-1/lesson-1"
           element={<Biology10Chapter1Lesson1 />}
         />
-<Route
-  path="/courses/biology-10/chapter-1/lesson-3"
-  element={<Biology10Chapter1Lesson3 />}
-/>
         <Route
-          path="/courses/biology-11"
-          element={<Biology11 />}
+          path="/courses/biology-10/chapter-1/lesson-2"
+          element={<Biology10Chapter1Lesson2 />}
+        />
+        <Route
+          path="/courses/biology-10/chapter-1/lesson-3"
+          element={<Biology10Chapter1Lesson3 />}
         />
 
-        <Route
-          path="/courses/biology-12"
-          element={<Biology12 />}
-        />
-
-        <Route
-          path="/courses/health-12"
-          element={<Health12 />}
-        />
+        <Route path="/courses/biology-11" element={<Biology11 />} />
+        <Route path="/courses/biology-12" element={<Biology12 />} />
+        <Route path="/courses/health-12" element={<Health12 />} />
       </Routes>
 
       <Footer />
